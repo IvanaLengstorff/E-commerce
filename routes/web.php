a963fd7b3aef0a27a302ca00a526059e9a5a970a
jsoliz064 <jsoliz064@gmail.com>
2023-10-17 03:31:04 -0400

    cruds

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,15 @@ use App\Http\Controllers\BranchController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\BrandsController;
-<<<<<<< HEAD
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WorkPositionController;
 use App\Http\Controllers\PaymentMethodController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\UserController;
-=======
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\WorkPositionController;
-use App\Http\Controllers\PaymentMethodController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SubcategoryController;
->>>>>>> 1ff6a04d7ee5d6c8b88c2223ceff6380047e9c8a
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
